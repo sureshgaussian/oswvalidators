d101300227db36b8c4bb1b8df3e781e33f677282
Ravi Gunti <rtgunti@gmail.com>
2020-07-08 18:42:00 +0200

    Update osw_node_presence.py
    Refactored to read multiple files

--- osw_node_presence.py
+++ osw_node_presence.py
@@ -16,6 +16,12 @@ def get_node_sidewalk_set(elems):
   elems : elements dictionary from df
   node_id_set : set of node ids
   sidewalk_elements : list of sidewalk elements
+  
+  Saves id of every node element to a list
+  Saves every sidewalk element to a list
+  We save the entire sidewalk element instead of just nodes.
+  This is to understand at which element exactly is the validation failing
+  
   '''
   node_id_set = set()
   sidewalk_elements = []
@@ -35,23 +41,26 @@ if __name__ == '__main__':
     path = "E:\oswvalidators\OSW\TestData"
     os.chdir(path)
     json_files = glob("*.json")
-    json_files.sort()
     print("Number of json files :", len(json_files))
     print(json_files)
     
-    for ind, file in enumerate(json_files):
-        with open(file) as json_data:
-            df = json.load(json_data)
+    # json_files = [i for i in json_files if 'broken' not in i]
     
-    print(df.keys())
-    elems = df['elements']
-    node_set, sidewalk_elements = get_node_sidewalk_set(elems)
-    print('Number of nodes : {}'.format(len(node_set)))
-    print('Number of Sidewalk elements : {}'.format(len(sidewalk_elements)))
+    for ind, file in enumerate(json_files):
+        print('-'*10)
+        print('File Name : {}'.format(file))
+        with open(file) as data_json:
+            data_dict = json.load(data_json)
+    
+        node_set, sidewalk_elements = get_node_sidewalk_set(data_dict['elements'])
+        print('Number of nodes : {}'.format(len(node_set)))
+        print('Number of Sidewalk elements : {}'.format(len(sidewalk_elements)))
 
-    missing_nodes = []
-    for sw_e in sidewalk_elements:
-        for node in sw_e['nodes']:
-            if(node not in node_set):
-                missing_nodes.append(node)
-    print('Validation : Failed -- Missing Nodes : {} from way id : {} '.format(missing_nodes, sw_e['id'])) if len(missing_nodes) else print('Validation : Success')
+        missing_nodes = []
+        #Compare each node in sidewalk element with the nodes set.
+        #TODO : Optimize matching criteria?
+        for sw_e in sidewalk_elements:
+            for node in sw_e['nodes']:
+                if(node not in node_set):
+                    missing_nodes.append(node)
+        print('Validation : Failed \nMissing Nodes : {} from way id : {} '.format(missing_nodes, sw_e['id'])) if len(missing_nodes) else print('Validation success')
