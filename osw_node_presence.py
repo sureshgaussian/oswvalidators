0e7c4042a37c9245147331545e218b4922e0d0fb
Ravi Gunti <rtgunti@gmail.com>
2020-07-08 17:38:59 +0200

    Adding Google Drive shared folder
    Adding files shared on Google Drive to use for scripts

---
+++ osw_node_presence.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 17:28:28 2020
+
+@author: rtgun
+"""
+
+import json
+import numpy as np
+import pandas as pd
+from pandas import json_normalize
+from glob import glob
