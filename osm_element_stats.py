af6a35115bfa0b9b0fa2826da0ca89a9779c6b23
Ravi Gunti <rtgunti@gmail.com>
2020-07-11 16:27:32 +0200

    Fixed typos

--- osm_element_stats.py
+++ osm_element_stats.py
@@ -14,11 +14,30 @@ import os
 def get_property_set(features_list):
     #Check keys in each of the properties element
     prop_set = set()
-    for ind, ft in enumerate(data_dict['features']):
+    prop_val_set = set()
+    for ind, ft in enumerate(features_list):
       prop_keys = list(ft['properties'].keys())
+      prop_vals = list(ft['properties'].values())
       prop_set.update(prop_keys)
-    print('Property set {}'.format(prop_set))    
+      prop_val_set.update(prop_vals)
+    print("Property set : ")
+    [print (i) for i in prop_set]
+    [print (i) for i in prop_val_set]
     return prop_set
+
+def get_property_dict(features_list):
+    #Check keys in each of the properties element
+    prop_dict = dict()
+    for ind, ft in enumerate(features_list):
+       for k, v in ft['properties'].items():
+           if(k in prop_dict):
+               if(v not in prop_dict[k]):
+                   prop_dict[k].append(v)
+           else:
+               prop_dict[k] = [v]
+    print(prop_dict.keys())
+    [print(k, v) for k,v in prop_dict.items()]
+    return prop_dict
 
 def get_geometry_type_set(features_list):
     #Get geometry type values from features list
@@ -26,7 +45,8 @@ def get_geometry_type_set(features_list):
     for ind, ft in enumerate(features_list):
       geom_type = ft['geometry']['type']
       geom_type_set.add(geom_type)
-    print('Geometry Type set : {}'.format(geom_type_set))
+    print("Geometry Type set : ")
+    [print (i) for i in geom_type_set]
     return geom_type_set
 
 if __name__ == '__main__':
@@ -37,7 +57,7 @@ if __name__ == '__main__':
     print("Number of json files :", len(json_files))
     print(json_files)
     
-    # json_files = [i for i in json_files if 'broken' not in i]
+    json_files = [i for i in json_files if 'redmon' in i]
     
     
     for ind, file in enumerate(json_files):
@@ -46,5 +66,6 @@ if __name__ == '__main__':
         with open(file) as data_json:
             data_dict = json.load(data_json)
     
-        property_set = get_property_set(data_dict['features'])        
-        geom_type_set = get_geometry_type_set(data_dict['features'])
+        # property_set = get_property_set(data_dict['features'])        
+        # geom_type_set = get_geometry_type_set(data_dict['features'])
+        property_dict = get_property_dict(data_dict['features']) 
