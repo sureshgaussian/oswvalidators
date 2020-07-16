83e509b6b62d9a0c9ccaf0542f9bdf2b7f0a5878
Ravi Gunti <rtgunti@gmail.com>
2020-07-16 20:01:58 +0200

    Added get_way_from_subgraph
    Function to get set of way ids from the given subgraph

--- node_connectivity.py
+++ node_connectivity.py
@@ -7,7 +7,9 @@ import os
 import pandas as pd
 import json
 from glob import glob
-
+import matplotlib.pylab as plt
+import networkx as nx
+import pickle
 
 
 '''
@@ -18,7 +20,7 @@ from glob import glob
 '''
 
 
-def get_coord_list(features_list):
+def get_ways(features_list):
     '''
     Returns list of list of coordinates.
     Outer list is the element
@@ -26,7 +28,8 @@ def get_coord_list(features_list):
     '''
     coord_list = []
     for elem in features_list:
-        coord_list.append(elem['geometry']['coordinates'])
+        if(elem['properties']['footway'] == 'sidewalk'):
+            coord_list.append(elem['geometry']['coordinates'])
     return coord_list
 
 def get_coord_dict(coord_list):
@@ -56,7 +59,22 @@ def get_coord_df(coord_list):
         df = df.append({'origin':str(elem[0]), 'dest':str(elem[-1])}, ignore_index=True)
     return df
 
-def get_disc_way_ids(coord_list, coord_dict):
+def plot_nodes_vs_ways(coord_list):
+    '''
+    #Nodes vs #Ways
+    '''
+    coord_freq_dict = dict()
+    for elem in coord_list:
+        if(len(elem) not in coord_freq_dict.keys()):
+            coord_freq_dict[str(len(elem))] = 1
+        else:
+            coord_freq_dict[str(len(elem))] += 1
+    x, y = zip(sorted(*coord_freq_dict.items()))
+    # print(x)
+    plt.plot(x, y)
+    plt.show()
+
+def get_isolated_way_ids(coord_list, coord_dict):
     '''
     Get way IDs to be discared (ways that are not part of any other ways)
     '''
@@ -72,31 +90,83 @@ def get_disc_way_ids(coord_list, coord_dict):
             disc_way_ids.update([id])
     return disc_way_ids
 
+def get_way_from_subgraph(sgraph, df):
+    '''
+    Networkx gives subgraphs. This function is to infer the 'way id' from the given subgraph.
+    Args : subgraph and df
+    Retruns : set of ways that belong to subgraph
+    '''   
+    way_set = set()
+    for ind, edge in enumerate(sgraph.edges):        
+        #Coordinates can be interchanged between origin and dest. Hence checking both ways
+        #either s1 or s2 gets a value which is index of the way from df, the given edge belongs to        
+        s1 = pd.Series((df['origin'] == edge[-1]) & (df['dest'] == edge[0]))
+        s2 = pd.Series((df['origin'] == edge[0]) & (df['dest'] == edge[-1]))        
+        s = s1[s1].index.tolist() + s2[s2].index.tolist()
+        way_set.update(s)        
+    return list(way_set)
+
 if __name__ == '__main__':
     path = "E:\oswvalidators\OSW\TestData"
+#     path = "E:\MAGDEBURG\SURESH\OSW\OSW\TestData" # Change the path here to a relative one in the git folder structure
     os.chdir(path)
     json_files = glob("*.geojson")
+    json_files = [i for i in json_files if 'redmond.' in i]
     print("Number of json files :", len(json_files))
-    print(json_files)
-    
-    json_files = [i for i in json_files if 'redmon_test' in i]
-    print(json_files)    
-    
     
     for ind, file in enumerate(json_files):
         print('-'*10)
-        print('File Name : {}'.format(file))
+        print('Processing File : {}'.format(file))
         with open(file) as data_json:
             data_dict = json.load(data_json)
     
-        coord_list = get_coord_list(data_dict['features'])
-        print(len(coord_list))
-        coord_dict = get_coord_dict(coord_list)
-        print(coord_dict)
+        Ways = get_ways(data_dict['features'])
         
-        df = get_coord_df(coord_list)
-        print("DataFrame : \n{}".format(df))
+        coord_dict = get_coord_dict(Ways)
+#         print("Coordinates Dictionary : \n", coord_dict)
         
-        disc_way_ids = get_disc_way_ids(coord_list, coord_dict)
-        # print(coord_dict.items())  
-        print(disc_way_ids)
+        df = get_coord_df(Ways)
+#         print("DataFrame : \n{}".format(df))
+        print("list : {} and df : {}".format(len(Ways), len(df.index)))
+        
+        isolated_way_ids = get_isolated_way_ids(Ways, coord_dict)
+#         print(coord_dict.items())  
+        Connected_Ways = Ways.copy()
+        for x in sorted(isolated_way_ids, reverse = True):  
+            del Connected_Ways[x] 
+    
+        #printing modified list
+        print("Number of ways (sidewalks) in the file : ", len(Ways))
+        print("Number of isolated ways: ", len(isolated_way_ids))
+        print("Number of Connected ways in the file : ", len(Connected_Ways))
+        
+        
+        Connected_df = get_coord_df(Connected_Ways)
+        Connected_FG = nx.from_pandas_edgelist(Connected_df, source='origin', target='dest')
+        print("Is Connected ? : ", nx.is_connected(Connected_FG))
+        print("Number of Connected Components : ", nx.number_connected_components(Connected_FG))
+            
+        cc = nx.connected_components(Connected_FG)
+        # for ind, x in enumerate(cc):
+        #     print('\n',ind,":", x)
+            
+        S = [Connected_FG.subgraph(c).copy() for c in nx.connected_components(Connected_FG)]
+                
+
+        pickle.dump(S, open('S.pkl', 'wb'))
+        S = pickle.load(open('S.pkl', 'rb'))
+        
+        pickle.dump(Connected_df, open('Connected_df.pkl', 'wb'))
+        Connected_df = pickle.load(open('Connected_df.pkl', 'rb'))
+        
+        pickle.dump(df, open('ways_df.pkl', 'wb'))
+        df = pickle.load(open('ways_df.pkl', 'rb'))
+                
+        
+        print(len(S))
+        print(S[1].edges)
+        nx.draw_networkx(S[1])
+        
+        sgraph = S[1]
+        ways_set = get_way_from_subgraph(S[1], df)
+        print("sgraph_ways {}".format(ways_set))
