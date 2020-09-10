83772c758e76362ebb512b155a02d4c720a14888
Rakesh Devalapally <45150828+devalapr@users.noreply.github.com>
2020-09-10 12:10:16 +0200

    Handled the modified OSW_Tags sheet v1.1
    AllOf
    AnyOf
    OneOf
    New format defined for the way dependencies are treated

--- Json Schema/generate_schema_from_excel.py
+++ Json Schema/generate_schema_from_excel.py
@@ -23,40 +23,64 @@ def build_properties(tags, types, df):
 			integer_types = integer_types + int_string + ','
 			
 		if types[index] == 'string':
-			string_enum = '"' + tags[index] + '": {"type":"string","enum":['
+			#string_enum = '"' + tags[index] + '": {"type":"string","enum":['
+			string_enum = '"' + tags[index] + '": {"type":"string"'
+			# There are some strings without any enumerated values. Example: Name and description
+			x = 0
 			for column in df.columns:
 				if not (pd.isna(row[column])):
-					string_enum = string_enum + '"' + row[column] + '",'
-			string_enum = string_enum[0:len(string_enum)-1] + ']}'
+					if x == 0:
+						string_enum = string_enum + ',"enum":["' + row[column] + '",'
+					else:
+						string_enum = string_enum + '"' + row[column] + '",'
+					x = x + 1
+			if x == 0:
+				string_enum = string_enum + '}'
+			else:
+				string_enum = string_enum[0:len(string_enum)-1] + ']}'
 			string_types = string_types + string_enum + ','
 
 	properties_string = integer_types + string_types[0:len(string_types)-1]
 	return properties_string
 
 
-def build_dependecies(dependencies):
+def build_dependecies(df):
 
-	children = dependencies["Tag"].values.tolist()
-	dependencies.drop(columns=['Tag'], inplace=True)
+	children = df["Tag"].values.tolist()
+	dependencies = df["Prereqs"].values.tolist()
 	dependency_string = ''
-	for index, row in dependencies.iterrows():
-		dp_string = '"' + children[index] + '": {"anyOf": ['
-		for column in dependencies.columns:
-			if not (pd.isna(row[column])):
-				parent_key = row[column].split('=')[0]
-				parent_value = row[column].split('=')[1]
-				child_string = '{"required": ["' + parent_key + '"], "properties":{'
+	
+	for i in range(len(dependencies)):
+		if 'AND' in dependencies[i]:
+			dp_string = '"' + children[i] + '": {"allOf": ['
+			dp_list = dependencies[i].split('AND')
+		elif 'OR' in dependencies[i]:
+			dp_string = '"' + children[i] + '": {"anyOf": ['
+			dp_list = dependencies[i].split('OR')
+		else:
+			dp_string = '"' + children[i] + '": {"anyOf": ['
+			dp_list = dependencies[i].split()
+		
+		child_string = ''
+		for x in dp_list:
+			parent_key = x.split('=')[0]
+			parent_value = x.split('=')[1]
+			if parent_value == '*':
+				child_string = child_string + '{"required": ["' + parent_key + '"]},'
+			else:
+				child_string = child_string + '{"required": ["' + parent_key + '"], "properties":{'
 				child_string = child_string + '"' + parent_key + '": {"type": "string", "const" : "' + parent_value + '"}}},'
 		dependency_string = dependency_string + dp_string + child_string[0:len(child_string)-1] + ']},'
 		
 	dependency_string = dependency_string[0:len(dependency_string)-1]
+	#print(dependency_string)
 
 	return dependency_string
 
 def generate_nodes_schema(tags, types, df, dependencies):
 	
-	print("Generating Nodes Schema")
 	point_properties = build_properties(tags, types, df)
+	print("Generating point dependencies")
 	point_dependencies = build_dependecies(dependencies)
 	
 	nodes_schema_string = '''{
@@ -254,45 +278,54 @@ def generate_ways_schema(tags, types, df, dependencies):
 	
 if __name__ == '__main__':
 	print("Reading the Excel file")
-	df = pd.read_excel('OSW_Tags.xlsx', sheet_name='Tags') 
-	dependencies = pd.read_excel('OSW_Tags.xlsx', sheet_name='Parents')
+	xl = pd.read_excel('OSW_Tags V1.1.xlsx', sheet_name='Tags')
+	dependencies = xl[['Tag','Prereqs','Geometry']]
 	
-	# Remove all those lines which do not have a dependency
+	# Remove all those lines which do not have a dependency and replace \n
 	dependencies = dependencies[dependencies['Prereqs'] != 'None']
-	#dependencies['Tag'] = [dependencies['Prereqs'].str.split('=').values[i][0] for i in range(dependencies.shape[0])]
-	dependencies.rename(columns = {'Tags':'Tag'}, inplace = True) 
+	dependencies['Prereqs'] = dependencies['Prereqs'].str.replace('\n','')
 
-	pointsdf = df[df['Geometry'] == 'Point'].sort_values(by=['type'])
-	pointsdf.reset_index(drop=True, inplace=True) # reset the index for future use
-	
-	linesdf = df[df['Geometry'] == 'LineString'].sort_values(by=['type'])
-	linesdf.reset_index(drop=True, inplace=True) # reset the index for future use
-	
+	# Get the required data to build Nodes Schema
+	pointsdf = xl[xl['Geometry'] == 'Point'].sort_values(by=['type'])
+	pointsdf.reset_index(drop=True, inplace=True)
 	point_tags = pointsdf.Tag.values.tolist()
 	point_types = pointsdf.type.values.tolist()
-	pointsdp = pd.merge(pointsdf.Tag, dependencies, on='Tag', how='inner')
-	pointsdf.drop(columns=['Tag','Geometry', 'type'], inplace=True)
+	pointsdf.drop(columns=['Tag','Prereqs','Geometry', 'type'], inplace=True)
 	
+	pointsdp = dependencies[dependencies['Geometry'] == 'Point']
+	pointsdp.drop(columns=['Geometry'], inplace=True)
+	pointsdp.reset_index(drop=True, inplace=True)
+	
+	# Get the required data to build Ways Schema
+	linesdf = xl[xl['Geometry'] == 'LineString'].sort_values(by=['type'])
+	linesdf.reset_index(drop=True, inplace=True)
 	line_tags = linesdf.Tag.values.tolist()
 	line_types = linesdf.type.values.tolist()
-	linesdp = pd.merge(linesdf.Tag, dependencies, on='Tag', how='inner')
-	linesdf.drop(columns=['Tag','Geometry', 'type'], inplace=True)
+	linesdf.drop(columns=['Tag','Prereqs','Geometry', 'type'], inplace=True)
 	
+	linesdp = dependencies[dependencies['Geometry'] == 'LineString']
+	linesdp.drop(columns=['Geometry'], inplace=True)
+	linesdp.reset_index(drop=True, inplace=True) 	
+
+	# Generate the schemas
 	nodes_schema = generate_nodes_schema(point_tags, point_types, pointsdf, pointsdp)
 	ways_schema = generate_ways_schema(line_tags, line_types, linesdf, linesdp)
 	
 	if json.loads(nodes_schema):
 		print('Generated Nodes schema Successfully \n')
+		formatted_nodes = json.dumps(json.loads(nodes_schema),indent=4)
 		nodes_file = open("Nodes_schema.json", "w")
-		nodes_file.write(nodes_schema)
+		nodes_file.write(formatted_nodes)
 		nodes_file.close()
 	else:
 		print('Error Generating Nodes Schema')
 	
 	if json.loads(ways_schema):
 		print('Generated Ways schema Successfully \n')
+		formatted_ways = json.dumps(json.loads(ways_schema),indent=4)
 		ways_file = open("Ways_schema.json", "w")
-		ways_file.write(ways_schema)
+		ways_file.write(formatted_ways)
 		ways_file.close()
 	else:
 		print('Error Generating Ways Schema')
+		
